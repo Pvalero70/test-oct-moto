c9dc714b73fa36ec07ff935b33f7254e53c506d8
isaac_mac <ich3869@gmail.com>
2022-03-16 23:03:26 -0600

    MOD: Send inv data flow changed

--- pos_customs/static/src/js/payment_screen.js
+++ pos_customs/static/src/js/payment_screen.js
@@ -1,18 +1,27 @@
-odoo.define('pos_customs.models_invoicing_data', function (require) {
+odoo.define('pos_customs.PaymentScreenC', function (require) {
 "use strict";
 console.log("Carga el chow");
-/*
+
 var models = require('point_of_sale.models');
 const PaymentScreen = require('point_of_sale.PaymentScreen');
 const Registries = require('point_of_sale.Registries');
 var rpc = require('web.rpc');
 
     const IIPaymentScreen = (PaymentScreen) =>
-        class extends PaymentScreen{};
+        class extends PaymentScreen{
             constructor() {
                 super(...arguments);
             }
+            setPaymentMethod(){
+                console.log("____Lanzando popup de Metodo de pago");
+            }
+            setCfdiUsage(){
+                console.log("____Lanzando popup de uso de CFDI");
+            }
+
+        };
+
     Registries.Component.extend(PaymentScreen, IIPaymentScreen);
     return IIPaymentScreen;
-    */
+
 });
