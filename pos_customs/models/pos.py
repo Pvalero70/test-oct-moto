887a9b68a153a5fa3d3f46da173b64da012761d5
isaac_mac <ich3869@gmail.com>
2022-03-17 23:06:51 -0600

    add: Invoice data flow ..

--- pos_customs/models/pos.py
+++ pos_customs/models/pos.py
@@ -23,3 +23,39 @@ class PosOrder(models.Model):
         _log.info("2 ========= VALS pos order .. ::: %s " % vals)
         # vals['to_invoice'] = True if ui_order.get('to_invoice') else False
         return vals
+    
+    l10n_mx_edi_usage = fields.Selection(
+        selection=[
+            ('G01', 'Adquisición de mercancías'),
+            ('G02', 'Devoluciones, descuentos o bonificaciones'),
+            ('G03', 'Gastos en general'),
+            ('I01', 'Construcciones'),
+            ('I02', 'Mobilario y equipo de oficina por inversiones'),
+            ('I03', 'Equipo de transporte'),
+            ('I04', 'Equipo de cómputo y accesorios'),
+            ('I05', 'Dados, troqueles, moldes, matrices y herramental'),
+            ('I06', 'Comunicaciones telefónicas'),
+            ('I07', 'Comunicaciones satelitales'),
+            ('I08', 'Otra maquinaria y equipo'),
+            ('D01', 'Honorarios médicos, dentales y gastos hospitalarios'),
+            ('D02', 'Gastos médicos por incapacidad o discapacidad'),
+            ('D03', 'Gastos funerales'),
+            ('D04', 'Donativos'),
+            ('D05', 'Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación)'),
+            ('D06', 'Aportaciones voluntarias al SAR'),
+            ('D07', 'Primas por seguros de gastos médicos'),
+            ('D08', 'Gastos de transportación escolar obligatoria.'),
+            ('D09', 'Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones.'),
+            ('D10', 'Pagos por servicios educativos (colegiaturas)'),
+            ('P01', 'Pör definir'),
+        ],
+        string="Uso",
+        default='P01')
+
+    def _prepare_invoice_vals(self):
+        vals = super(PosOrder, self)._prepare_invoice_vals()
+        # vals['l10n_mx_edi_payment_method_id'] = self.payment_method_id.l10n_mx_edi_payment_method_id.id
+        vals['l10n_mx_edi_usage'] = self.l10n_mx_edi_usage
+        # vals['pricelist_id'] = self.pricelist_id
+        _log.info("===================== VALORES PARA LA FACTURA... %s" % vals)
+        return vals
