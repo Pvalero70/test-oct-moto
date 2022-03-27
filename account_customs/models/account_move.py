e8b469699cec324a1adcfa903ef248911b6f012f
isaac_mac <ich3869@gmail.com>
2022-03-27 17:23:20 -0600

    Add. asign journal id by producto category in accesories

--- account_customs/models/account_move.py
+++ account_customs/models/account_move.py
@@ -17,9 +17,18 @@ class AccountMoveCustoms(models.Model):
                 continue
             # Buscamos en las lineas productos que pertenezcan a cierta categoría, por ahora R y S.
             prods_line = inv.invoice_line_ids.mapped('product_id')
-            s_products = prods_line.filtered(lambda p: p.categ_id.display_name.upper().find('ACCESORIOS') >= 0 or p.categ_id.display_name.upper().find('REFACCIONES') >= 0)
+            s_products = prods_line.filtered(lambda p: p.categ_id.display_name.upper().find(
+                'ACCESORIOS') >= 0 or p.categ_id.display_name.upper().find('REFACCIONES') >= 0)
             if s_products:
                 # Identificamos el diario con code in ['s', 'S']
                 journal_id = self.env['account.journal'].search([('code', 'in', ['s', 'S'])], limit=1)
                 inv.journal_id = journal_id.id
+                break
+            r_products = prods_line.filtered(lambda p: p.categ_id.display_name.upper().find(
+                'REPARACI') >= 0)
+            if r_products:
+                # Identificamos el diario con code in ['R', 'r']
+                journal_id = self.env['account.journal'].search([('code', 'in', ['r', 'R'])], limit=1)
+                inv.journal_id = journal_id.id
+                break
         return invoices
