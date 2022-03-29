56ad9140ef9dc1b3c71221729225f6b9e4b285ba
isaac_mac <ich3869@gmail.com>
2022-03-28 23:03:36 -0600

    ADD: brand to product template

--- inventory_customs/models/product_brand.py
+++ inventory_customs/models/product_brand.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 
 class BrandProduct(models.Model):
     _name = 'product.brand'
+    _description = "Model that saves the product brand records"
 
     name = fields.Char()
     brand_image = fields.Binary()
