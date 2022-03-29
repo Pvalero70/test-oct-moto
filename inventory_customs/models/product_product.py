5d91044a55905c35648e8a5ad590786e18f766d1
isaac_mac <ich3869@gmail.com>
2022-03-28 21:58:13 -0600

    ADD: distinctive product type field

--- inventory_customs/models/product_product.py
+++ inventory_customs/models/product_product.py
@@ -8,7 +8,7 @@ _log = logging.getLogger("___name: %s" % __name__)
 class ProductTemplateCus(models.Model):
     _inherit = "product.template"
 
-    product_inv_categ = fields.Selection([
+    product_invoice_categ = fields.Selection([
         ('moto', 'Motocicleta'),
         ('refaccion', 'Refacción'),
         ('accesorio', 'Accesorio'),
