b6f26776e4e9834188a4d309b4454d41bd090ef7
isaac_mac <ich3869@gmail.com>
2022-03-29 23:42:54 -0600

    Modf. hide or show motor number col in entry

--- inventory_customs/models/stock.py
+++ inventory_customs/models/stock.py
@@ -8,5 +8,11 @@ _log = logging.getLogger("___name: %s" % __name__)
 class StockMoveLineC(models.Model):
     _inherit = "stock.move.line"
 
+    is_moto = fields.Boolean(string="Es motocicleta", compute="_compute_is_moto", store=False)
     motor_number = fields.Char(string="Número de motor")
 
+    def _compute_is_moto(self):
+        if self.move_id.product_id and self.move_id.product_id.product_inv_categ and self.move_id.product_id.product_inv_categ == "moto":
+            self.is_moto = True
+        else:
+            self.is_moto = False
