87d38bb7109ad13278f797c0ff8301daa794b597
isaac_mac <ich3869@gmail.com>
2022-03-29 23:01:54 -0600

    Add. first step to add motor number

--- inventory_customs/models/stock.py
+++ inventory_customs/models/stock.py
@@ -8,4 +8,12 @@ _log = logging.getLogger("___name: %s" % __name__)
 class StockMoveLineC(models.Model):
     _inherit = "stock.move.line"
 
+    is_moto = fields.Boolean(string="Es motocicleta", compute="_compute_is_moto", store=False)
     motor_number = fields.Char(string="Número de motor")
+
+    @api.depends('move_id')
+    def _compute_is_moto(self):
+        if self.move_id.product_id and self.move_id.product_id.product_inv_categ and self.move_id.product_id.product_inv_categ == "moto":
+            self.is_moto = True
+        else:
+            self.is_moto = False
