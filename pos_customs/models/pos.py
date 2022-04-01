58c290c268a6902188fe0f49b15f2489f19717f3
isaac_mac <ich3869@gmail.com>
2022-03-31 22:13:49 -0600

    Fix: view for salesman no found

--- pos_customs/models/pos.py
+++ pos_customs/models/pos.py
@@ -26,7 +26,7 @@ class PosOrder(models.Model):
         vals['to_invoice'] = True if ui_order.get('to_invoice') else False
         return vals
 
-    salesman_id = fields.Many2one('res.users', string="Vendedor", compute="_compute_salesman", store=True)
+    salesman_id = fields.Many2one('res.users', string="Ejecutivo", compute="_compute_salesman", store=True)
     cfdi_payment_term_id = fields.Many2one('account.payment.term', 'Terminos de pago')
     payment_method_id = fields.Many2one('pos.payment.method', "Metodo de Pago", compute="get_payment_method",
                                         store=True)
