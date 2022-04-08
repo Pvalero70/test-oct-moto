bda332b7d2d88af0533780301addf6a09bb440a8
isaac_mac <ich3869@gmail.com>
2022-04-08 01:37:37 -0500

    MODF: change the method selection of the journal by location and category

--- account_customs/models/__init__.py
+++ account_customs/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# from . import product_category
 from . import account_journal
 from . import pos_order
 
--- account_customs/models/pos_order.py
+++ account_customs/models/pos_order.py
@@ -10,19 +10,29 @@ class PosOrderC(models.Model):
 
     def _prepare_invoice_vals(self):
         vals = super(PosOrderC, self)._prepare_invoice_vals()
-        # Revisamos aquí el diario.
-        # Revisamos los produtos de la orden de venta
-        # product_id = self.lines.mapped('product_id').filtered(
-        #     lambda p: p.categ_id.target_journal_id is not False)
-        # if product_id:
-        #     journal_id = product_id[:1].categ_id.target_journal_id
-        #     # Asignamos el diario a los datos de la factura.
-        #     if journal_id:
-        #         _log.info("\n\n Asignando el diario: %s " % journal_id)
-        #         vals['journal_id'] = journal_id.id
+        if not self.lines:
+            return vals
+        first_product_category = self.lines[:1].product_id.categ_id
+        pc_loc_src_id = self.config_id.picking_type_id.default_location_src_id
 
-        # ====== CAMBIO DE LOGICA ==========
-        # OBtenemos la caterogía del primer producto.
-        # Obtenemos la ubicación de origen ( config_id.picking_type_id.default_location_src_id)
+        # Search journal by product category, location source and company.
+        domain = [('company_id', '=', self.company_id.id),
+                  ('c_product_category_id', '=', first_product_category.id),
+                  ('c_location_id', '=', pc_loc_src_id.id)
+                  ]
+        journal_id = self.env['account.journal'].search(domain, limit=1)
+
+        # If the journal was found, set it.
+        if journal_id:
+            vals['journal_id'] = journal_id.id
+        elif first_product_category.parent_id:
+            # If the journal wasn't found, search using the parent category, if exists.
+            domain = [('company_id', '=', self.company_id.id),
+                      ('c_product_category_id', '=', first_product_category.parent_id.id),
+                      ('c_location_id', '=', pc_loc_src_id.id)
+                      ]
+            journal_id = self.env['account.journal'].search(domain, limit=1)
+            if journal_id:
+                vals['journal_id'] = journal_id.id
 
         return vals
