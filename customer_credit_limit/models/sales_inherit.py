4ef3c566846308cb844488a4bac079570b8761c6
isaac_mac <ich3869@gmail.com>
2022-04-12 21:41:18 -0500

    MoDF. Customer credit limit.. email template added

--- customer_credit_limit/models/sales_inherit.py
+++ customer_credit_limit/models/sales_inherit.py
@@ -12,22 +12,23 @@ class sale_order(models.Model):
     @api.onchange('partner_id')
     def _compute_total_customer_limit_total(self):
         # Buscar el adeudo en facturas ya timbradas.
-        inv_domain = [
-            ('move_type', '=', 'out_invoice'),
-            ('partner_id', '=', self.partner_id.id),
-            ('edi_state', '=', "sent"),
-            ('amount_residual', '>', 0),
-        ]
-        partner_invoice_signed_ids = self.env['account.move'].search(inv_domain)
-        amount_due = 0
-        if self.currency_id and self.currency_id != self.company_id.currency_id:
-            for inv in partner_invoice_signed_ids:
-                amount_due += inv.amount_residual._convert(inv.amount_residual, inv.company_id.currency_id, self.company_id, inv.invoice_date or fields.Date.today())
-        else:
-            amount_due = sum(partner_invoice_signed_ids.mapped('amount_residual'))
-        # Total disponible es su limite menos lo que ya debe.
-        total_available = self.partner_id.credit_limit - amount_due
-        self.update({'sale_credit_limit_customer_total': total_available})
+        for sale in self:
+            inv_domain = [
+                ('move_type', '=', 'out_invoice'),
+                ('partner_id', '=', sale.partner_id.id),
+                ('edi_state', '=', "sent"),
+                ('amount_residual', '>', 0),
+            ]
+            partner_invoice_signed_ids = self.env['account.move'].search(inv_domain)
+            amount_due = 0
+            if sale.currency_id and sale.currency_id != sale.company_id.currency_id:
+                for inv in partner_invoice_signed_ids:
+                    amount_due += inv.amount_residual._convert(inv.amount_residual, inv.company_id.currency_id, sale.company_id, inv.invoice_date or fields.Date.today())
+            else:
+                amount_due = sum(partner_invoice_signed_ids.mapped('amount_residual'))
+            # Total disponible es su limite menos lo que ya debe.
+            total_available = sale.partner_id.credit_limit - amount_due
+            sale.update({'sale_credit_limit_customer_total': total_available})
 
     @api.onchange('partner_id', 'order_line')
     def _compute_approve_needed(self):
