26bc553dfe3c80545dd3d44e09721b7613a9abbe
isaac_mac <ich3869@gmail.com>
2022-04-14 00:52:05 -0500

    MOdf, sale order list filter by state (print)

--- customer_credit_limit/static/src/js/saleOrderList.js
+++
@@ -1,7 +0,0 @@
-odoo.define('customer_credit_limit.saleOrderList',function (require){
-    'use strict';
-    const PosComponent = require('point_of_sale.PosComponent');
-    const Registries = require('point_of_sale.Registries');
-    ['state','=','sale']
-
-});
