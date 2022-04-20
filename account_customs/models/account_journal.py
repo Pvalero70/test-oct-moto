6eecda0f4331c4050cfebddcf7dce2b0acfdd0a3
IsaacChavezArroyo <Isaac>
2022-04-20 13:23:41 -0500

    add. m2m field

--- account_customs/models/account_journal.py
+++ account_customs/models/account_journal.py
@@ -8,6 +8,8 @@ _log = logging.getLogger("___name: %s" % __name__)
 class AccountJournalC(models.Model):
     _inherit = "account.journal"
 
+    c_product_category_ids = fields.Many2many('product.category', 'journal_product_categ_rel',
+                                              'journalid', 'categid', string="Categorías de productos")
     c_product_category_id = fields.Many2one('product.category', string="Categoría de productos")
     c_location_id = fields.Many2one('stock.location')
 
