12847aa7496a5340dedd9ac4ea4c8e8b2b37f375
isaac_mac <ich3869@gmail.com>
2022-04-21 00:25:52 -0500

    Modf selection about journal id clean not used fields

--- account_customs/models/account_journal.py
+++ account_customs/models/account_journal.py
@@ -10,7 +10,6 @@ class AccountJournalC(models.Model):
 
     c_product_category_ids = fields.Many2many('product.category', 'journal_product_categ_rel',
                                               'journalid', 'categid', string="Categorías de productos")
-    c_product_category_id = fields.Many2one('product.category', string="Categoría de productos")
     c_location_id = fields.Many2one('stock.location')
 
 
