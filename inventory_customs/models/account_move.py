7d9f837cfe1de9ad57237e5706e14d9f7b95a600
isaac_mac <ich3869@gmail.com>
2022-04-28 00:02:42 -0500

    Add. inherit inv templ and add fields

--- inventory_customs/models/account_move.py
+++ inventory_customs/models/account_move.py
@@ -10,20 +10,14 @@ class AccountMoveItt(models.Model):
 
     def _get_invoiced_lot_values_tt(self, product_id=None):
         # Poner aquí la restricción de la compañia.
-        _log.info("\nCalculando para el producto:: %s" % product_id)
         if self.move_type != "out_invoice" or self.state == 'draft':
-            _log.info("\nSe sale en la primer condición")
             return False
         sale_lines = self.invoice_line_ids.sale_line_ids
-        # sale_orders = sale_lines.order_id
         # Filter lines for specific product
         stock_move_lines = sale_lines.move_ids.filtered(lambda r: r.state == 'done').move_line_ids.filtered(lambda r: r.product_id.id == product_id.id)
-        _log.info("\nLineas a iterar::: %s " % stock_move_lines)
         data = []
         for line in stock_move_lines:
-            _log.info("\nCalculando linea ::: %s" % line)
             if not line.product_id.product_inv_categ or not line.product_id.product_inv_categ in ["moto", "Moto"]:
-                _log.info("\nBrincando producto.")
                 continue
             # Put: color; inv num,
             data.append({
