e2af83859d822d68def6b04f3e70b4e681fe4ae2
IsaacChavezArroyo <Isaac>
2022-04-28 14:15:51 -0500

    DEBUG: receipt doesn't shown

--- pos_customs/static/src/js/orderReceipt.js
+++ pos_customs/static/src/js/orderReceipt.js
@@ -10,8 +10,9 @@ odoo.define('pos_customs.OrderReceipt', function(require) {
         get sale_seller() {
 //            Get the first line to get user of sale.order object.
             console.log(this);
-            var seller_name = this.orderlines[0].sale_order_origin_id.user_id[1];
-            return seller_name;
+//            var seller_name = this.orderlines[0].sale_order_origin_id.user_id[1];
+//            return seller_name;
+            return "Holas";
         }
     }
 
