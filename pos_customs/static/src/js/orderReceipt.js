5f26f3872242ad10b812fe7b71eb74d023418ef4
IsaacChavezArroyo <Isaac>
2022-04-29 17:35:44 -0500

    Clean: comments

--- pos_customs/static/src/js/orderReceipt.js
+++ pos_customs/static/src/js/orderReceipt.js
@@ -9,25 +9,16 @@ odoo.define('pos_customs.OrderReceipt', function(require) {
         class extends OrderReceipt {
         get sale_seller() {
 //            Get the first line to get user of sale.order object.
-//            console.log("THIS ==>> ");
-//            console.log(this);
-//            console.log(" ORDER LINES :: == >>> ");
-//            console.log(this.orderlines)
             var order_liness = this.orderlines[0];
-//            console.log(order_liness);
             if(order_liness["sale_order_origin_id"] == undefined){
-//                console.log("SALE ORDER ORI NO FOUND");
                 return false;
             }
             var sale_order = order_liness.sale_order_origin_id;
-//            console.log(sale_order);
             if(sale_order["user_id"] != undefined){
-//                console.log("definido");
                 var seller_name = this.orderlines[0].sale_order_origin_id.user_id[1];
                 return seller_name;
             }
             else{
-//                console.log("indefinido");
                 return false;
             }
         }
