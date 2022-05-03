6d1f81371b7bfbb22170a0c6c2d9a353bbc6b8ae
gerardomr8 <gerardo.mixteco@quadit.mx>
2022-05-03 15:12:35 -0500

    [IMP] q_repair_extend: visibility buttons, refactor code picking

--- q_repair_extend/models/__init__.py
+++ q_repair_extend/models/__init__.py
@@ -3,3 +3,4 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import repair_order
+from . import stock_picking
--- q_repair_extend/models/repair_order.py
+++ q_repair_extend/models/repair_order.py
@@ -12,16 +12,28 @@ class RepairOrderInherit(models.Model):
     picking_ids = fields.Many2many('stock.picking', compute='_compute_picking_ids', string='Receptions', copy=False)
     operation_id = fields.Many2one('stock.picking.type', 'Operation Type', required=True)
     repair_confirm = fields.Boolean('Repair confirm', default=False, copy=False)
-    
-    def action_repair_confirm(self):
-        res = super(RepairOrderInherit, self).action_repair_confirm()
+    picking_confirm = fields.Boolean('Picking confirm', default=False, copy=False)
+    invisible_button = fields.Boolean('Invisible button', copy=False, compute="_invisible_button")
+
+    def _invisible_button(self):
+        for record in self:
+            if record.repair_confirm or record.picking_confirm:
+                if record.repair_confirm and record.picking_confirm:
+                    record.invisible_button = False
+                else:
+                    record.invisible_button = True
+            else:
+                record.invisible_button = False
+
+    def action_validate(self):
+        res = super(RepairOrderInherit, self).action_validate()
         self.repair_confirm = True
         return res
     
     @api.onchange('location_id')
     def _dominios(self):
         for record in self:
-            b ={'domain': {'operation_id': ['|',('default_location_dest_id', '=', record.location_id.id),('default_location_src_id', '=', record.location_id.id),('code', '=', 'incoming')]}}
+            b ={'domain': {'operation_id': ['|','|',('default_location_dest_id', '=', record.location_id.id),('default_location_src_id', '=', record.location_id.id),('code', '=', 'incoming')]}}
             return b
     
     def _compute_picking_ids(self):
@@ -62,19 +74,17 @@ class RepairOrderInherit(models.Model):
                 }
             )
 
-            for op in record.operations:
-                line =  (0, 0,{
-                    'name': op.product_id.name,
-                    'product_id': op.product_id.id,
-                    'product_uom_qty': op.product_uom_qty,
-                    'product_uom': op.product_uom.id,
-                    'description_picking': op.product_id.name,
-                    'picking_id': record_stock.id,
-                    'location_id': self.env.ref('stock.stock_location_suppliers').id or False,
-                    'location_dest_id': location.lot_stock_id.id,
-                    'lot_ids': [op.lot_id.id],
-                    })
-                lines_vals.append(line)
+            lines_vals =  [(0, 0,{
+                'name': record.product_id.name,
+                'product_id': record.product_id.id,
+                'product_uom_qty': record.product_qty,
+                'product_uom':record.product_uom.id,
+                'description_picking': record.product_id.name,
+                'picking_id': record_stock.id,
+                'location_id': self.env.ref('stock.stock_location_suppliers').id or False,
+                'location_dest_id': location.lot_stock_id.id,
+                'lot_ids': [record.lot_id.id],
+                })]
 
             record_stock.sudo().write({'move_ids_without_package': lines_vals})
             record_stock.action_confirm()            
@@ -84,6 +94,7 @@ class RepairOrderInherit(models.Model):
         """ This function returns an action that display existing picking orders of given purchase order ids. When only one found, show the picking immediately.
         """
         self.ensure_one()
+        print(pickings)
         result = self.env["ir.actions.actions"]._for_xml_id('stock.action_picking_tree_all')
         result['context'] = {'origin': self.name}
         if not pickings or len(pickings) > 1:
