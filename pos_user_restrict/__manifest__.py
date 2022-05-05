77acc20b2d554ca80510d7e79b1478b14a35e47b
Alejandro <alexgama9817@gmail.com>
2022-05-05 17:17:47 -0500

    Discount lines

--- pos_user_restrict/__manifest__.py
+++ pos_user_restrict/__manifest__.py
@@ -18,7 +18,10 @@
     ],
     'data': [
         'security/pos_user_restrict_security.xml',
+        'security/ir.model.access.csv',
+        'security/user_discount_security.xml',
         'views/res_users_views.xml',
+        'views/res_users_discount_views.xml',
     ],
     'demo': [
         'demo/point_of_sale_demo.xml',
---
+++ pos_user_restrict/models/res_users_discount.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+class ResUserInheritDiscount(models.Model):
+    _inherit = 'res.users'
+
+    discount_ids = fields.One2many('res.users.discount','seller_id',String="Lista de Descuentos")
+
+
+class ResUsersDiscount(models.Model):
+    _name = 'res.users.discount'
+
+    _description = "Model that saves discounts on product categories"
+
+    seller_id = fields.Many2one('res.users' , 'Vendedor')
+    discount_permitted = fields.integer('Descuento permitido')
+    category_ids = fields.Many2many('product.category' , 'Categorias')
+
