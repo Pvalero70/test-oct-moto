f6cb93e58d3cba4948a0450654a1dada4ce66ff3
Alejandro <alexgama9817@gmail.com>
2022-05-05 20:43:10 -0500

    Permisos sobre descuentos

--- pos_user_restrict/models/res_users_discount.py
+++ pos_user_restrict/models/res_users_discount.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+import api as api
 
-from odoo import fields, models
+from odoo import api, fields, models
 import logging
 
 
@@ -29,10 +30,10 @@ class ResUsersDiscount(models.Model):
         _logger.debug('resultado de grupo : %s : y grupos : %s', res, grupos)
         return super(ResUsersDiscount, self).write(values)
 
-
-    def create(self, values):
+    @api.model
+    def create(self, vals):
         _logger.debug('Create a %s with vals %s', self._name, values)
         res = self.env.user.has_group('base.user_discount_agente_group')
         grupos = self.env.user.groups
         _logger.debug('resultado de grupo : %s : y grupos : %s', res, grupos)
-        return 0
+        return super(ResUsersDiscount, self).create(vals)
