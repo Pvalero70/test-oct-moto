e2fc1844c1b107f27bc37d253994db61e720bf38
AledroG <alexgama9817@gmail.com>
2022-05-07 12:40:35 -0500

    discount restrict

--- pos_user_restrict/models/res_users_discount.py
+++ pos_user_restrict/models/res_users_discount.py
@@ -4,13 +4,13 @@ import logging
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
 
-
 _logger = logging.getLogger(__name__)
+
 
 class ResUserInheritDiscount(models.Model):
     _inherit = 'res.users'
 
-    discount_ids = fields.One2many('res.users.discount','seller_id',String="Lista de Descuentos")
+    discount_ids = fields.One2many('res.users.discount', 'seller_id', String="Lista de Descuentos")
 
 
 class ResUsersDiscount(models.Model):
@@ -18,12 +18,12 @@ class ResUsersDiscount(models.Model):
 
     _description = "Model that saves discounts on product categories"
 
-    seller_id = fields.Many2one('res.users' , 'Vendedor',)
+    seller_id = fields.Many2one('res.users', 'Vendedor', )
     discount_permitted = fields.Integer('Descuento permitido')
-    category_ids = fields.Many2many(comodel_name='product.category' , string='Categorias')
-    location_ids = fields.Many2many(comodel_name = 'stock.location', string="Ubicacion")
+    category_ids = fields.Many2many(comodel_name='product.category', string='Categorias')
+    location_ids = fields.Many2many(comodel_name='stock.location', string="Ubicacion")
 
-    def _restrictions_discounts(self,seller,discount_permitted):
+    def _restrictions_discounts(self, seller, discount_permitted):
         descuento_20 = seller.has_group('pos_user_restrict.user_discount_agente_group')
         _logger.info('resultado pertenece a  grupo : %s : y vendedor %s', descuento_20, seller, )
 
@@ -35,7 +35,6 @@ class ResUsersDiscount(models.Model):
 
         if discount_permitted > 20 and descuento_20 == False:
             raise ValidationError(_('Advertencia!, El descuento maximo permitido es 5%.'))
-
 
     def write(self, vals):
         _logger.info('Write Method a %s with vals %s', self._name, vals)
@@ -50,33 +49,26 @@ class ResUsersDiscount(models.Model):
         if 'seller_id' in vals:
             seller = self.env['res.users'].search([('id', '=', vals['seller_id'])], limit=1)
 
-
-        self._restrictions_discounts(seller,discount_permitted)
+        self._restrictions_discounts(seller, discount_permitted)
 
         if 'category_ids' in vals:
             lis_category_ids = vals['category_ids'][0][2]
             descuentos_lines = self.env['res.users.discount'].search([('seller_id', '=', seller.id)])
-
-
-
-
 
         return super(ResUsersDiscount, self).write(vals)
 
     @api.model_create_multi
     def create(self, vals):
         _logger.info('Create a %s with vals %s', self._name, vals)
-        #descuento_20 = self.env.user.has_group('pos_user_restrict.user_discount_agente_group')
-        #grupos = self.env.user.groups_id
+        # descuento_20 = self.env.user.has_group('pos_user_restrict.user_discount_agente_group')
+        # grupos = self.env.user.groups_id
         for i in range(len(vals)):
             seller = self.env['res.users'].search([('id', '=', vals[i]['seller_id'])], limit=1)
             permitted_discount = vals[i]['discount_permitted']
 
-            self._restrictions_discounts(seller,permitted_discount)
+            self._restrictions_discounts(seller, permitted_discount)
 
-
-
-            categorias_ids =  vals[i]['category_ids'][0][2]
+            categorias_ids = vals[i]['category_ids'][0][2]
             _logger.info('Categorys id = %s', categorias_ids)
             descuentos_lines = self.env['res.users.discount'].search([('seller_id', '=', vals[i]['seller_id'])])
             for j in range(len(categorias_ids)):
@@ -84,13 +76,11 @@ class ResUsersDiscount(models.Model):
                     _logger.info('Categoriass id = %s', categorias_ids[j])
                     _logger.info('Antes del for %s', descuentos_lines[k].category_ids)
                     if categorias_ids[j] in [cat.id for cat in descuentos_lines[k].category_ids]:
-                        categoria_rep = self.env['product.category'].search([('id', '=', categorias_ids[j] )], limit=1)
-                        raise ValidationError(_('Advertencia!, La categoria %s ya esta en otro registro',categoria_rep.name))
-
-
+                        categoria_rep = self.env['product.category'].search([('id', '=', categorias_ids[j])], limit=1)
+                        raise ValidationError(
+                            _('Advertencia!, La categoria %s ya esta en otro registro', categoria_rep.name))
 
         return super(ResUsersDiscount, self).create(vals)
-
 
 
 class SaleOrderInherit(models.Model):
@@ -98,4 +88,17 @@ class SaleOrderInherit(models.Model):
 
     @api.onchange('order_line')
     def _onchange_order(self):
-        _logger.info("SALE ORDER::Valores recibidos %s",self.order_line)
+        _logger.info("SALE ORDER::Cantidad de lineas ", len(self.order_line))
+        discount_lines = self.env['res.users.discount'].search([('seller_id', '=', self.env.user.id)], limit=1)
+        for order in self.order_line:
+            if order.product_template_id and order.product_template_id.categ_id:
+                _logger.info("Linea con valores %s, y categoria %s", order.product_template_id.name,
+                             order.product_template_id.categ_id.name)
+
+                for discount_line in discount_lines:
+                    for categ in discount_line:
+                        if categ.id == order.product_template_id.categ_id.id and order.discount > discount_line.discount_permitted:
+                            raise ValidationError(
+                                _('Advertencia!, El descuento permitido en %s para categoria %s es %s\%',
+                                  order.product_template_id.name, categ.name, discount_line.discount_permitted))
+        _logger.info("SALE ORDER::Valores recibidos %s", self.order_line)
