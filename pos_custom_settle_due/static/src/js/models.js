3b64d4849a31fe46eaa8f704e4c1106e6ef30b5f
glvega <gerardo.lopez@yuju.io>
2022-05-08 17:05:42 -0500

    Override valida order method

--- pos_custom_settle_due/static/src/js/models.js
+++ pos_custom_settle_due/static/src/js/models.js
@@ -4,6 +4,7 @@ odoo.define('pos_custom_settle_due.PaymentScreen', function (require) {
     const PaymentScreen = require('pos_settle_due.PaymentScreen');
     const Registries = require('point_of_sale.Registries');
     const { float_is_zero } = require('web.utils');
+    const { patch } = require('web.utils');
 
     patch(PaymentScreen, "static patch", { 
 
