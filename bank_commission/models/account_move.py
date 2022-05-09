61324c13ad611e36af71ed3b46fda481326b6431
IsaacCA <IsaacCA>
2022-05-09 18:36:06 -0500

    Add: create invoice

--- bank_commission/models/account_move.py
+++ bank_commission/models/account_move.py
@@ -6,26 +6,26 @@ from dateutil.relativedelta import relativedelta
 import logging
 _log = logging.getLogger("___name: %s" % __name__)
 
-
-class AccountMoveBc(models.Model):
-    _inherit = "account.move"
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        # OVERRIDE
-        invoices = super(AccountMoveBc, self).create(vals_list)
-        _log.info("\n TODAS LAS FACTURAS ::: %s" % invoices)
-        for inv in invoices:
-            _log.info("\n Factura a revisar::: (%s) $s " % inv.name, inv)
-            if not inv.pos_order_ids:
-                continue
-            _log.info("\n Factura del punto de venta")
-            invoice_pos_payment_methods = inv.pos_order_ids.mapped('payment_method_id').filtered(lambda bcm: bcm.bank_commission_method != False)
-            if not invoice_pos_payment_methods:
-                continue
-            self.create_bc_inv(inv)
-        return invoices
-
-    @api.model
-    def create_bc_inv(self, inv):
-        _log.info("\n Creando comisión para la factura:: %s " % inv)
+#
+# class AccountMoveBc(models.Model):
+#     _inherit = "account.move"
+#
+#     @api.model_create_multi
+#     def create(self, vals_list):
+#         # OVERRIDE
+#         invoices = super(AccountMoveBc, self).create(vals_list)
+#         _log.info("\n TODAS LAS FACTURAS ::: %s" % invoices)
+#         for inv in invoices:
+#             _log.info("\n Factura a revisar::: (%s) $s " % inv.name, inv)
+#             if not inv.pos_order_ids:
+#                 continue
+#             _log.info("\n Factura del punto de venta")
+#             invoice_pos_payment_methods = inv.pos_order_ids.mapped('payment_method_id').filtered(lambda bcm: bcm.bank_commission_method != False)
+#             if not invoice_pos_payment_methods:
+#                 continue
+#             self.create_bc_inv(inv)
+#         return invoices
+#
+#     @api.model
+#     def create_bc_inv(self, inv):
+#         _log.info("\n Creando comisión para la factura:: %s " % inv)
--- bank_commission/models/pos.py
+++ bank_commission/models/pos.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+import pytz
 from odoo.exceptions import ValidationError, UserError
 from dateutil.relativedelta import relativedelta
 import logging
@@ -26,9 +27,54 @@ class PosOrderBc(models.Model):
         # Creating new invoice for commission bank
         _log.info("\n Creando factura para.. ")
         for payment in self.payment_ids.filtered(lambda x: x.payment_method_id.bank_commission_method is not False):
-            self._create_bc_invoice(payment)
+            if not self._create_bc_invoice(payment):
+                continue
         return res
 
-    @api.model
     def _create_bc_invoice(self, payment):
         _log.info("\n Creando factura para el pago:: %s" % payment)
+        # Localiza el método de calculo de la comisión
+        bc_method = payment.payment_method_id.bank_commission_method
+        bc_amount = payment.payment_method_id.bank_commission_amount
+        bc_product_id = payment.payment_method_id.bank_commission_product_id
+        # Calcula el total de la factura.
+        inv_total = 0
+        if bc_method and bc_method == "percentage":
+            inv_total = payment.amount * bc_amount
+        elif bc_method and bc_method == "fixed":
+            inv_total = bc_amount
+
+        # Creamos la factura.
+        timezone = pytz.timezone(self._context.get('tz') or self.env.user.tz or 'UTC')
+        vals = {
+            'invoice_origin': self.name,
+            'journal_id': self.session_id.config_id.invoice_journal_id.id, ###
+            'move_type': 'out_invoice',
+            'ref': self.name,
+            'partner_id': self.partner_id.id,
+            'partner_bank_id': self._get_partner_bank_id(),
+            'currency_id': self.pricelist_id.currency_id.id,
+            'invoice_user_id': self.user_id.id,
+            'invoice_date': self.date_order.astimezone(timezone).date(),
+            'invoice_cash_rounding_id': self.config_id.rounding_method.id
+        }
+        invoice_lines = ((0, None, {
+            'product_id': bc_product_id.id,
+            'quantity': 1,
+            'discount': 0,
+            'price_unit': inv_total,
+            'name': bc_product_id.display_name,
+            'tax_ids': bc_product_id.taxes_id.ids,
+            'product_uom_id': bc_product_id.uom_id.id,
+        }))
+        vals['invoice_line_ids'] = invoice_lines
+        invoice = self.env['account.move'].sudo().with_company(self.company_id).with_context(
+            default_move_type='out_invoice').create(vals)
+        message = _(
+            "Esta factura ha sido creada desde el punto de venta: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (
+                  self.id, self.name)
+        invoice.message_post(body=message)
+        return invoice
+
+
+
