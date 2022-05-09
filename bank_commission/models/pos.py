26c7b84ee43a658c757af58e74bd1de0673f4a80
IsaacCA <IsaacCA>
2022-05-09 09:02:53 -0500

    Modf. commission bank method

--- bank_commission/models/pos.py
+++ bank_commission/models/pos.py
@@ -10,9 +10,9 @@ _log = logging.getLogger("___name: %s" % __name__)
 class PosPaymentMethodBc(models.Model):
     _inherit = "pos.payment.method"
 
-    # bank_commission_method = fields.Selection([
-    #     (False, ""),
-    #     ("percentage", "Porcentaje"),
-    #     ("fixed", "Monto fijo")
-    # ], string="Método de calculo comisión", default="")
+    bank_commission_method = fields.Selection([
+        (False, ""),
+        ("percentage", "Porcentaje"),
+        ("fixed", "Monto fijo")
+    ], string="Método de calculo comisión")
     bank_commission_amount = fields.Float(string="Monto de comisión")
