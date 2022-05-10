e2053f76be694eceb222eae257bfa8b6a8d7574d
glvega <gerardo.lopez@yuju.io>
2022-05-09 23:14:01 -0500

    Sobre escribe create order methods

--- pos_custom_settle_due/static/src/js/models.js
+++ pos_custom_settle_due/static/src/js/models.js
@@ -1,7 +1,7 @@
 odoo.define('pos_custom_settle_due.PaymentScreen', function (require) {
     'use strict';
 
-    const PaymentScreen = require('pos_settle_due.PaymentScreen');
+    const PaymentScreen = require('pos_customs.PaymentScreenC');
     const Registries = require('point_of_sale.Registries');
     const { float_is_zero } = require('web.utils');
     // const { patch } = require('web.utils');
@@ -16,55 +16,54 @@ odoo.define('pos_custom_settle_due.PaymentScreen', function (require) {
         class extends PaymentScreen {
             
 
-            async validateOrder() {
-                console.log('###Sobre escribe###')
+            // async validateOrder() {
+            //     console.log('###Sobre escribe###')
 
-                const order = this.currentOrder;
+            //     const order = this.currentOrder;
                 
-                if (!order.is_payment_invoice){
-                    return super.validateOrder(...arguments);
-                }
-                else{
-                    const change = order.get_change();
-                    const paylaterPaymentMethod = this.env.pos.payment_methods.filter(
-                        (method) =>
-                            this.env.pos.config.payment_method_ids.includes(method.id) && method.type == 'pay_later'
-                    )[0];
-                    const existingPayLaterPayment = order
-                        .get_paymentlines()
-                        .find((payment) => payment.payment_method.type == 'pay_later');
+            //     if (!order.is_payment_invoice){
+            //         return super.validateOrder(...arguments);
+            //     }
+            //     else{
+            //         const change = order.get_change();
+            //         const paylaterPaymentMethod = this.env.pos.payment_methods.filter(
+            //             (method) =>
+            //                 this.env.pos.config.payment_method_ids.includes(method.id) && method.type == 'pay_later'
+            //         )[0];
+            //         const existingPayLaterPayment = order
+            //             .get_paymentlines()
+            //             .find((payment) => payment.payment_method.type == 'pay_later');
                     
-                    console.log(order)
-                    console.log(change)
-                    console.log(paylaterPaymentMethod)
-                    console.log(existingPayLaterPayment)
+            //         console.log(order)
+            //         console.log(change)
+            //         console.log(paylaterPaymentMethod)
+            //         console.log(existingPayLaterPayment)
     
-                    // this.currentOrder.finalized = true;
+            //         // this.currentOrder.finalized = true;
     
-                    // order.destroy({ reason: 'abandon' });
+            //         // order.destroy({ reason: 'abandon' });
     
-                    // this.showScreen(this.nextScreen);
+            //         // this.showScreen(this.nextScreen);
     
-                    // return
-                    console.log(arguments)
+            //         // return
+            //         console.log(arguments)
     
-                    const res = super.validateOrder(...arguments);
+            //         const res = super.validateOrder(...arguments);
     
-                    console.log("Despues de la validacion")
+            //         console.log("Despues de la validacion")
     
-                    // const paylaterPayment = order.add_paymentline(paylaterPaymentMethod);
-                    // paylaterPayment.set_amount(change);
+            //         // const paylaterPayment = order.add_paymentline(paylaterPaymentMethod);
+            //         // paylaterPayment.set_amount(change);
     
-                    return res
-                }
+            //         return res
+            //     }
 
                 
-            };
+            // };
             async _finalizeValidation() {
                 
                 console.log("FINALIZE VALIDATION")
                 this.currentOrder.prueba = 123
-                this.currentOrder.to_invoice = [1, 2]
                 console.log(this.currentOrder)
 
                 return super._finalizeValidation(...arguments);
