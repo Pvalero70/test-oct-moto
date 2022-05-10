701111ac0dc73ffd107645dafc18d20fed249ccd
AledroG <alexgama9817@gmail.com>
2022-05-10 18:55:55 -0500

    new module migrate

--- pos_user_restrict/__manifest__.py
+++ pos_user_restrict/__manifest__.py
@@ -18,10 +18,7 @@
     ],
     'data': [
         'security/pos_user_restrict_security.xml',
-        'security/user_discount_security.xml',
-        'security/ir.model.access.csv',
         'views/res_users_views.xml',
-        'views/res_users_discount_views.xml',
     ],
     'demo': [
         'demo/point_of_sale_demo.xml',
