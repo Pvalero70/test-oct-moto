f499fdea440a3e2b904ae5d10856c72c0d88ab4c
AledroG <alexgama9817@gmail.com>
2022-05-12 18:36:04 -0500

    Update repair_mechanic.py

--- repair_mechanic/models/repair_mechanic.py
+++ repair_mechanic/models/repair_mechanic.py
@@ -31,7 +31,7 @@ class RepairMechanic(models.Model):
     @api.depends('first_name','second_name','first_ap','second_ap')
     def _compute_name(self):
         for rec in self:
-            nombre = rec.first_name + " "+ (str(rec.second_name+" ") if rec.second_name else "") + rec.first_ap +" "+ (str(rec.second_ap) if rec.second_ap else "")
+            nombre = rec.first_name + " "+ str((rec.second_name+" ") if rec.second_name else "") + rec.first_ap +" "+ str((rec.second_ap) if rec.second_ap else "")
 
             rec.name_computed = nombre
 
