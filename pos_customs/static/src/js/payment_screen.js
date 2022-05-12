a0d0830ae330d1cf66ce06ad463dd8706e039cc0
glvega <gerardo.lopez@yuju.io>
2022-05-11 21:42:30 -0500

    Actualiza pos customs with payment complement

--- pos_customs/static/src/js/payment_screen.js
+++ pos_customs/static/src/js/payment_screen.js
@@ -26,81 +26,86 @@ var rpc = require('web.rpc');
 
 
             async _finalizeValidation() {
-            if ((this.currentOrder.is_paid_with_cash() || this.currentOrder.get_change()) && this.env.pos.config.iface_cashdrawer) {
-                this.env.pos.proxy.printer.open_cashbox();
-            }
+                
+                console.log("Sobre escribe finalize validation")
+                console.log(this.currentOrder)
 
-            this.currentOrder.initialize_validation_date();
-            this.currentOrder.finalized = true;
-
-            let syncedOrderBackendIds = [];
-
-            try {
-                if (this.currentOrder.is_to_invoice()) {
-                   this.currentOrder.to_invoice = [$("#cfdi_usage_sel").val(),$("#payment_termss_selection").val()];
-                    // this.currentOrder.cfdi_usage = $("#cfdi_usage_sel").val();
-
-                    syncedOrderBackendIds = await this.env.pos.push_and_invoice_order(
-                        this.currentOrder
-                    );
-                } else {
-                    syncedOrderBackendIds = await this.env.pos.push_single_order(this.currentOrder);
+                if ((this.currentOrder.is_paid_with_cash() || this.currentOrder.get_change()) && this.env.pos.config.iface_cashdrawer) {
+                    this.env.pos.proxy.printer.open_cashbox();
                 }
-            } catch (error) {
-                if (error.code == 700)
-                    this.error = true;
 
-                if ('code' in error) {
-                    // We started putting `code` in the rejected object for invoicing error.
-                    // We can continue with that convention such that when the error has `code`,
-                    // then it is an error when invoicing. Besides, _handlePushOrderError was
-                    // introduce to handle invoicing error logic.
-                    await this._handlePushOrderError(error);
-                } else {
-                    // We don't block for connection error. But we rethrow for any other errors.
-                    if (isConnectionError(error)) {
-                        this.showPopup('OfflineErrorPopup', {
-                            title: this.env._t('Connection Error'),
-                            body: this.env._t('Order is not synced. Check your internet connection'),
-                        });
+                this.currentOrder.initialize_validation_date();
+                this.currentOrder.finalized = true;
+
+                let syncedOrderBackendIds = [];
+
+                try {
+                    if (this.currentOrder.is_to_invoice()) {
+                        this.currentOrder.to_invoice = [$("#cfdi_usage_sel").val(),$("#payment_termss_selection").val()];
+                            // this.currentOrder.cfdi_usage = $("#cfdi_usage_sel").val();
+
+                            syncedOrderBackendIds = await this.env.pos.push_and_invoice_order(
+                                this.currentOrder
+                            );
                     } else {
-                        throw error;
+                        console.log("Push single order")
+                        syncedOrderBackendIds = await this.env.pos.push_single_order(this.currentOrder);
+                    }
+                } catch (error) {
+                    if (error.code == 700)
+                        this.error = true;
+
+                    if ('code' in error) {
+                        // We started putting `code` in the rejected object for invoicing error.
+                        // We can continue with that convention such that when the error has `code`,
+                        // then it is an error when invoicing. Besides, _handlePushOrderError was
+                        // introduce to handle invoicing error logic.
+                        await this._handlePushOrderError(error);
+                    } else {
+                        // We don't block for connection error. But we rethrow for any other errors.
+                        if (isConnectionError(error)) {
+                            this.showPopup('OfflineErrorPopup', {
+                                title: this.env._t('Connection Error'),
+                                body: this.env._t('Order is not synced. Check your internet connection'),
+                            });
+                        } else {
+                            throw error;
+                        }
+                    }
+                }
+                if (syncedOrderBackendIds.length && this.currentOrder.wait_for_push_order()) {
+                    const result = await this._postPushOrderResolve(
+                        this.currentOrder,
+                        syncedOrderBackendIds
+                    );
+                    if (!result) {
+                        await this.showPopup('ErrorPopup', {
+                            title: this.env._t('Error: no internet connection.'),
+                            body: this.env._t('Some, if not all, post-processing after syncing order failed.'),
+                        });
+                    }
+                }
+
+                this.showScreen(this.nextScreen);
+
+                // If we succeeded in syncing the current order, and
+                // there are still other orders that are left unsynced,
+                // we ask the user if he is willing to wait and sync them.
+                if (syncedOrderBackendIds.length && this.env.pos.db.get_orders().length) {
+                    const { confirmed } = await this.showPopup('ConfirmPopup', {
+                        title: this.env._t('Remaining unsynced orders'),
+                        body: this.env._t(
+                            'There are unsynced orders. Do you want to sync these orders?'
+                        ),
+                    });
+                    if (confirmed) {
+                        // NOTE: Not yet sure if this should be awaited or not.
+                        // If awaited, some operations like changing screen
+                        // might not work.
+                        this.env.pos.push_orders();
                     }
                 }
             }
-            if (syncedOrderBackendIds.length && this.currentOrder.wait_for_push_order()) {
-                const result = await this._postPushOrderResolve(
-                    this.currentOrder,
-                    syncedOrderBackendIds
-                );
-                if (!result) {
-                    await this.showPopup('ErrorPopup', {
-                        title: this.env._t('Error: no internet connection.'),
-                        body: this.env._t('Some, if not all, post-processing after syncing order failed.'),
-                    });
-                }
-            }
-
-            this.showScreen(this.nextScreen);
-
-            // If we succeeded in syncing the current order, and
-            // there are still other orders that are left unsynced,
-            // we ask the user if he is willing to wait and sync them.
-            if (syncedOrderBackendIds.length && this.env.pos.db.get_orders().length) {
-                const { confirmed } = await this.showPopup('ConfirmPopup', {
-                    title: this.env._t('Remaining unsynced orders'),
-                    body: this.env._t(
-                        'There are unsynced orders. Do you want to sync these orders?'
-                    ),
-                });
-                if (confirmed) {
-                    // NOTE: Not yet sure if this should be awaited or not.
-                    // If awaited, some operations like changing screen
-                    // might not work.
-                    this.env.pos.push_orders();
-                }
-            }
-        }
 
 
         };
