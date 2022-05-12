240bc04afabeed995cec0ab5a807043f436a1e17
AledroG <alexgama9817@gmail.com>
2022-05-12 00:21:03 -0500

    inherit same function

--- pos_order_restrict/models/res_users_discount.py
+++ pos_order_restrict/models/res_users_discount.py
@@ -277,25 +277,23 @@ class SaleOrderInherit(models.Model):
                 if len(dict['errores']) > 0:
                     raise UserError(dict['errores'])
 
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
+        res = super(SaleOrderInherit, self).action_confirm()
+        return res
 
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
-        self.write(self._prepare_confirmation_values())
 
-        # Context key 'default_name' is sometimes propagated up to here.
-        # We don't need it and it creates issues in the creation of linked records.
-        context = self._context.copy()
-        context.pop('default_name', None)
-
-        self.with_context(context)._action_confirm()
-        if self.env.user.has_group('sale.group_auto_done_setting'):
-            self.action_done()
-        return True
-
+    def action_quotation_send(self):
+        _logger.info("SALE ORDER:: mi funcion enviar")
+        if self.state == 'draft':
+            if self.need_discount_aprove == False:
+                if self.gerente_discount_id:
+                    if self.gerente_discount_id.id != self.env.user.id:
+                        raise UserError(_("Advertencia, El gerente de descuentos debe aprobarla"))
+                else:
+                    dict = self.restrictions_discount()
+                    if len(dict['errores']) > 0:
+                        raise UserError(dict['errores'])
+        res = super(SaleOrderInherit, self).action_quotation_send()
+        return res
 
 class ResCompanyInherit(models.Model):
     _inherit = 'res.company'
