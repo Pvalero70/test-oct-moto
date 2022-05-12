4687f8df9a111e3173b7f725b8fc4c7d4a7bc2cb
glvega <gerardo.lopez@yuju.io>
2022-05-11 23:47:28 -0500

    Actualiza pos customs with payment complement

--- pos_customs/models/payment.py
+++ pos_customs/models/payment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
 import logging
 _log = logging.getLogger("___name: %s" % __name__)
 
@@ -18,3 +19,18 @@ class AccountPayment(models.Model):
     def crear_pago_pos(self, values):
         _log.info("## Intenta crear pago from pos##")
         _log.info(values)
+        customer = values.get('customer')
+        journal_id = None
+        amount = 0
+        for pay in values.get('payments', []):
+            if pay.get('method', {}).get('type') != 'pay_later':
+                journal_id = pay.get('method', {}).get('id')
+                amount = pay.get('amount')
+        invoice = values.get('invoice')
+        self.create({
+            "patner_id" : customer.get('id'),
+            "date" : datetime.now().strftime("%Y-%m-%d"),
+            "journal_id" : journal_id,
+            "amount" : amount,
+            "ref" : invoice.get('name')
+        })
