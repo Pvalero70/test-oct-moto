b0ba2cf2d108e0ec5cce67d95b8cc987e028de63
glvega <gerardo.lopez@yuju.io>
2022-05-12 23:25:31 -0500

    Sobre escribe metodo

--- pos_customs/models/account_move.py
+++ pos_customs/models/account_move.py
@@ -12,4 +12,9 @@ class AccountMove(models.Model):
     def js_assign_outstanding_line(self, line_id):
         _log.info(self)
         _log.info(line_id)
+        move_line = self.env['account.move.line'].browse(line_id)
+        _log.info(move_line.account_id.id)
+        _log.info(move_line.account_id.name)
+        _log.info(move_line.debit)
+        _log.info(move_line.credit)
         return
