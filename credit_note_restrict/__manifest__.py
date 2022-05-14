ec270637e47e7897f7aadc908e8f0f8bfd52e607
AledroG <alexgama9817@gmail.com>
2022-05-13 23:42:11 -0500

    Module credit_note restrict

---
+++ credit_note_restrict/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2018 Garazd Creation (<https://garazd.biz>)
+# @author: Yurii Razumovskyi (<support@garazd.biz>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+{
+    'name': 'Restricciones de notas de credito',
+    'version': '15.0.1.0.1',
+    'category': 'Ventas',
+    'author': 'Ing. Alejandro Garcia Magaña',
+    'website': '',
+    'license': 'LGPL-3',
+    'summary': 'Restricciones de contabilidad',
+
+    'depends': [
+        'account',
+        'pos_sale',
+    ],
+    'data': [
+        'security/credit_note_security.xml',
+    ],
+    'demo': [],
+    'external_dependencies': {
+    },
+    'support': 'support@garazd.biz',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
