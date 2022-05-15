6d3fedc2d21d027a36e8e376a4b9cbfe42d2a606
AledroG <alexgama9817@gmail.com>
2022-05-15 12:34:46 -0500

    Update RefundButtonHide.js

--- credit_note_restrict/static/src/js/RefundButtonHide.js
+++ credit_note_restrict/static/src/js/RefundButtonHide.js
@@ -1,37 +1,24 @@
 odoo.define('credit_note_restrict.RefundButtonHide', function (require) {
     'use strict';
 
-    const PosComponent = require('point_of_sale.PosComponent');
-    const ProductScreen = require('point_of_sale.ProductScreen');
+    const ButtonRefund = require('point_of_sale.RefundButton');
     const Registries = require('point_of_sale.Registries');
-    const { useListener } = require('web.custom_hooks');
 
-    class RefundButton extends PosComponent {
-        constructor() {
-            super(...arguments);
-            useListener('click', this._onClick);
-            console.log("En mi funcion")
-        }
-        _onClick() {
-            const customer = this.env.pos.get_order().get_client();
-            const searchDetails = customer ? { fieldName: 'CUSTOMER', searchTerm: customer.name } : {};
-            this.trigger('close-popup');
-            this.showScreen('TicketScreen', {
-                ui: { filter: 'SYNCED', searchDetails },
-                destinationOrder: this.env.pos.get_order(),
-            });
-        }
-    }
-    RefundButton.template = 'point_of_sale.RefundButton';
 
-    ProductScreen.addControlButton({
-        component: RefundButton,
-        condition: function () {
-            return true;
-        },
-    });
+    const POSRefundButtonCustomHide = (ButtonRefund) =>
+        class extends ButtonRefund {
+            constructor() {
+                console.log("js:: en mi funcion")
 
-    Registries.Component.add(RefundButton);
+                super(...arguments);
 
-    return RefundButton;
+               
+
+
+            }
+        };
+
+    Registries.Component.extend(ButtonRefund, POSRefundButtonCustomHide);
+
+    return ButtonRefund;
 });
