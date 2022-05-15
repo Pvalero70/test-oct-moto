dcecd659d01d57438fae335fc174b4f21d689f99
AledroG <alexgama9817@gmail.com>
2022-05-15 12:25:08 -0500

    Update RefundButtonHide.js

--- credit_note_restrict/static/src/js/RefundButtonHide.js
+++ credit_note_restrict/static/src/js/RefundButtonHide.js
@@ -11,6 +11,7 @@ odoo.define('credit_note_restrict.RefundButtonHide', function (require) {
                 console.log("js:: en mi funcion")
 
                 super(...arguments);
+                $('.control-button')[1].style.visibility = "hidden";
                 var botones_control = $('.control-button')[1];
                 console.log("Botones control")
                 console.log(botones_control)
