8cda55475836d616f1bede6e9427e0d06e1519cb
AledroG <alexgama9817@gmail.com>
2022-05-15 11:05:01 -0500

    add js code

---
+++ credit_note_restrict/static/src/js/RefundButtonHide.js
@@ -0,0 +1,20 @@
+odoo.define('credit_note_restrict.RefundButtonHide', function (require) {
+    'use strict';
+
+    const ButtonRefund = require('point_of_sale.RefundButton);
+    const Registries = require('point_of_sale.Registries');
+
+
+    const POSRefundButtonCustomHide = (ButtonRefund) =>
+        class extends ButtonRefund {
+            constructor() {
+                super(...arguments);
+                console.log("js:: en mi funcion")
+
+            }
+        };
+
+    Registries.Component.extend(ButtonRefund, POSRefundButtonCustomHide);
+
+    return ButtonRefund;
+});
