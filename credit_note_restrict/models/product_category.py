da43a2da7344c4065297897202e253a5b3c5a5ba
AledroG <alexgama9817@gmail.com>
2022-05-14 23:12:42 -0500

    Update product_category.py

--- credit_note_restrict/models/product_category.py
+++ credit_note_restrict/models/product_category.py
@@ -3,7 +3,7 @@ import logging
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError, Warning
-#import xml.etree.ElementTree as etree
+# import xml.etree.ElementTree as etree
 from lxml import etree
 
 _logger = logging.getLogger(__name__)
@@ -12,9 +12,8 @@ _logger = logging.getLogger(__name__)
 class ResUserInheritDiscount(models.Model):
     _inherit = 'product.category'
 
-    account_credit_note_id = fields.Many2one('account.account',"Cuenta de nota de credito")
+    account_credit_note_id = fields.Many2one('account.account', "Cuenta de nota de credito")
     account_discount_id = fields.Many2one('account.account', "Cuenta de descuento o bonificacion")
-
 
 
 class AccountMoveInherit(models.Model):
@@ -23,33 +22,38 @@ class AccountMoveInherit(models.Model):
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
         res = super(AccountMoveInherit, self).fields_view_get(view_id=view_id, view_type=view_type,
-                                                  toolbar=toolbar, submenu=submenu)
+                                                              toolbar=toolbar, submenu=submenu)
 
         context = self.env.context
-        _logger.info("ACCOUNT MOVE MODEL:: view type %s, permiso factura client %s, context default_move_type %s",view_type, self.env.user.has_group('credit_note_restrict.factura_client_group'),context.get('default_move_type'))
+        _logger.info("ACCOUNT MOVE MODEL:: view type %s, permiso factura client %s, context default_move_type %s",
+                     view_type, self.env.user.has_group('credit_note_restrict.factura_client_group'),
+                     context.get('default_move_type'))
         doc = etree.XML(res['arch'])
 
-
-        if view_type in ['form','tree']:
-            if (not self.env.user.has_group('credit_note_restrict.factura_client_group')) and context.get('default_move_type') == 'out_invoice': #Facturas de clientes
+        if view_type in ['form', 'tree']:
+            if (not self.env.user.has_group('credit_note_restrict.factura_client_group')) and context.get(
+                    'default_move_type') == 'out_invoice':  # Facturas de clientes
                 for node_form in doc.xpath("//form"):
                     node_form.set("create", 'false')
                 for node_form in doc.xpath("//tree"):
                     node_form.set("create", 'false')
 
-            if (not self.env.user.has_group('credit_note_restrict.credit_note_client_group')) and context.get('default_move_type') == 'out_refund': #Notas de credito en clientes
+            if (not self.env.user.has_group('credit_note_restrict.credit_note_client_group')) and context.get(
+                    'default_move_type') == 'out_refund':  # Notas de credito en clientes
                 for node_form in doc.xpath("//form"):
                     node_form.set("create", 'false')
                 for node_form in doc.xpath("//tree"):
                     node_form.set("create", 'false')
 
-            if (not self.env.user.has_group('credit_note_restrict.factura_proveedor_group')) and context.get('default_move_type') == 'in_invoice': #Facturas en proveedores
+            if (not self.env.user.has_group('credit_note_restrict.factura_proveedor_group')) and context.get(
+                    'default_move_type') == 'in_invoice':  # Facturas en proveedores
                 for node_form in doc.xpath("//form"):
                     node_form.set("create", 'false')
                 for node_form in doc.xpath("//tree"):
                     node_form.set("create", 'false')
 
-            if (not self.env.user.has_group('credit_note_restrict.reembolso_proveedor_group')) and context.get('default_move_type') == 'in_refund': #reembolsos en proveedores
+            if (not self.env.user.has_group('credit_note_restrict.reembolso_proveedor_group')) and context.get(
+                    'default_move_type') == 'in_refund':  # reembolsos en proveedores
                 for node_form in doc.xpath("//form"):
                     node_form.set("create", 'false')
                 for node_form in doc.xpath("//tree"):
@@ -61,10 +65,65 @@ class AccountMoveInherit(models.Model):
 class AccountTranzientReversal(models.TransientModel):
     _inherit = 'account.move.reversal'
 
-    reason_select = fields.Selection([ ('devolucion', 'Devolucion'),('descuento', 'Descuento o Bonificacion'),('otro', 'Otro')],'Type', default='devolucion')
+    reason_select = fields.Selection(
+        [('devolucion', 'Devolucion'), ('descuento', 'Descuento o Bonificacion'), ('otro', 'Otro')], 'Type',
+        default='devolucion')
 
     def reverse_moves(self):
         _logger.info('REVERSE_MOVES:: en mi funcion')
 
-        res = super(AccountTranzientReversal, self).reverse_moves()
-        return res
+        self.ensure_one()
+        moves = self.move_ids
+        _logger.info("REVERSE_MOVES:: MOVES = %s",moves)
+        # Create default values.
+        default_values_list = []
+        for move in moves:
+            default_values_list.append(self._prepare_default_reversal(move))
+
+        batches = [
+            [self.env['account.move'], [], True],  # Moves to be cancelled by the reverses.
+            [self.env['account.move'], [], False],  # Others.
+        ]
+        for move, default_vals in zip(moves, default_values_list):
+            is_auto_post = bool(default_vals.get('auto_post'))
+            is_cancel_needed = not is_auto_post and self.refund_method in ('cancel', 'modify')
+            batch_index = 0 if is_cancel_needed else 1
+            batches[batch_index][0] |= move
+            batches[batch_index][1].append(default_vals)
+
+        # Handle reverse method.
+        moves_to_redirect = self.env['account.move']
+        for moves, default_values_list, is_cancel_needed in batches:
+            new_moves = moves._reverse_moves(default_values_list, cancel=is_cancel_needed)
+
+            if self.refund_method == 'modify':
+                moves_vals_list = []
+                for move in moves.with_context(include_business_fields=True):
+                    moves_vals_list.append(
+                        move.copy_data({'date': self.date if self.date_mode == 'custom' else move.date})[0])
+                new_moves = self.env['account.move'].create(moves_vals_list)
+
+            moves_to_redirect |= new_moves
+
+        self.new_move_ids = moves_to_redirect
+
+        # Create action.
+        action = {
+            'name': _('Reverse Moves'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+        }
+        if len(moves_to_redirect) == 1:
+            action.update({
+                'view_mode': 'form',
+                'res_id': moves_to_redirect.id,
+                'context': {'default_move_type': moves_to_redirect.move_type},
+            })
+        else:
+            action.update({
+                'view_mode': 'tree,form',
+                'domain': [('id', 'in', moves_to_redirect.ids)],
+            })
+            if len(set(moves_to_redirect.mapped('move_type'))) == 1:
+                action['context'] = {'default_move_type': moves_to_redirect.mapped('move_type').pop()}
+        return action
