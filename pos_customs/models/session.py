67f428f8995de99ad402e169e25f88adeb0de93a
glvega <gerardo.lopez@yuju.io>
2022-05-16 21:56:31 -0500

    elimina lineas vacias

--- pos_customs/models/session.py
+++ pos_customs/models/session.py
@@ -154,11 +154,14 @@ class PosSession(models.Model):
                         if line.debit == 0 and line.credit == 0:
                             line.unlink()
                     if not debit_move_id.line_ids:
+                        _logger.info("Se elimina move porque no tiene lineas")
                         debit_move_id.unlink()
                     else:
                         debit_move_id.action_post()
             
+            _logger.info(debit_move_id)
             if debit_move_id and debit_move_id.state != 'posted':
+                _logger.info("Se intenta procesar el move id")
                 try:
                     debit_move_id.action_post()
                 except Exception as e:                    
