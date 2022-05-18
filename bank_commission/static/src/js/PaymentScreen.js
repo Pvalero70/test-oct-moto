cb126baa4c1c4be29de94ed4d69e6065164b39dc
IsaacCA <IsaacCA>
2022-05-18 00:15:56 -0500

    Fix: flow in pos, update payment line and commission line

--- bank_commission/static/src/js/PaymentScreen.js
+++ bank_commission/static/src/js/PaymentScreen.js
@@ -14,28 +14,25 @@ const PaymentScreenBc = (PaymentScreen) =>
     class extends PaymentScreen{
         constructor(){
             super(...arguments);
-            console.log("Extiende Paymentscreen BC1");
-            console.log(this);
         }
 
         addNewPaymentLine({ detail: paymentMethod }) {
-
-            console.log("COMM METHD");
-            console.log(paymentMethod);
-            console.log(" ORDER LINES :: ");
-            console.log(this.currentOrder.get_orderlines());
             if (paymentMethod.bank_commission_method){
-
-                let product_byid = this.env.pos.db.get_product_by_id(paymentMethod.bank_commission_product_id[0]);
-//                console.log("PRODUCTO");
-//                console.log(product_byid);
-
+                let product_id = paymentMethod.bank_commission_product_id[0];
+                let product_byid = this.env.pos.db.get_product_by_id(product_id);
+                let oline = this.currentOrder.get_orderlines().find(line => line.product.id === product_id);
+                console.log("ORDER LINEA:: ");
+                console.log(oline);
+                let current_com = 0;
+                if (oline && this.currentOrder.get_due() > 0){
+                    current_com = oline.price;
+                }
                 let price = 0;
                 if (paymentMethod.bank_commission_method == "percentage"){
-                    let cline = this.currentOrder.get_orderlines().find(line => line.paymentMethod == paymentMethod.id);
-                    console.log("LINEA:: ");
-                    console.log(cline);
-                    price = paymentMethod.bank_commission_amount;
+                    let total_due = this.currentOrder.get_total_with_tax()-current_com-this.currentOrder.get_total_paid()+ this.currentOrder.get_rounding_applied();
+                    price = total_due * (paymentMethod.bank_commission_amount/100);
+//                     Restar lo que ya se tenga de comision.
+//                    price = this.currentOrder.get_due() * (paymentMethod.bank_commission_amount/100);
                 }
                 if (paymentMethod.bank_commission_method == "fixed"){
                     price = paymentMethod.bank_commission_amount;
@@ -46,21 +43,16 @@ const PaymentScreenBc = (PaymentScreen) =>
                     lst_price: price,
                     extras: {price_manually_set: true,paymentMethod:paymentMethod.id},
                 });
-                console.log(" YA FUE AGREGADO EL PRODUCTO .. ");
-                console.log(this);
-
             }
             return super.addNewPaymentLine(...arguments);
         }
 
         deletePaymentLine(event) {
-            console.log(" BORRA NDO EL MÉTODO DE PAGO ..   ");
             const { cid } = event.detail;
             const pline = this.paymentLines.find((line) => line.cid === cid);
-            console.log(" PLINE .. ");
-            console.log(pline);
             super.deletePaymentLine(...arguments);
             let order = this.currentOrder;
+            // TODO: Al quitar una es necesario reecalcular las demás para volver agregar la comisión.
             if (pline.payment_method.bank_commission_method){
                 let product = pline.payment_method.bank_commission_product_id[0];
                 let line = order.get_orderlines().find(line => line.product.id === product && line.paymentMethod === pline.payment_method.id);
@@ -71,8 +63,16 @@ const PaymentScreenBc = (PaymentScreen) =>
         }
 
         _updateSelectedPaymentline(){
-            console.log("Actualizando lineas.. ")
             super._updateSelectedPaymentline(...arguments);
+            let pline = this.selectedPaymentLine;
+            let order = this.currentOrder;
+            let oline = order.get_orderlines().find(line => line.paymentMethod === pline.payment_method.id && line.product.id === pline.payment_method.bank_commission_product_id[0]);
+            if (oline) {
+                let com_total = pline.amount * (pline.payment_method.bank_commission_amount/100);
+                oline.set_unit_price(com_total);
+                // Obtenemos el total de la compra y le restamos la comisión actual.
+                // reecalculamos la comisión
+            }
         }
     };
 
