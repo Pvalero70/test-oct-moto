94ea732288b640ce1a2cdc461cd358910a066218
gerardomr8 <gerardo.mixteco@quadit.mx>
2022-05-19 18:00:00 -0500

    [IMP] q_pdv_order_repair: transfer to SO

--- q_pdv_orders_repair/static/src/js/ProductItem.js
+++ q_pdv_orders_repair/static/src/js/ProductItem.js
@@ -1,9 +1,9 @@
 odoo.define('q_pdv_orders_repair.ProductItem', function(require) {
     'use strict';
-    
+
     const Registries = require('point_of_sale.Registries');
     var ProductItem = require('point_of_sale.ProductItem');
-
+    
     const ProductItemInherit = ProductItem => class extends ProductItem {
         add_product_cart(product) {
             this.env.pos.get_order().add_product(product, {
@@ -13,4 +13,5 @@ odoo.define('q_pdv_orders_repair.ProductItem', function(require) {
     };
     Registries.Component.extend(ProductItem, ProductItemInherit);
     return ProductItem;
+
 });
