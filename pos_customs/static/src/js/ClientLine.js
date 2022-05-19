668ba774e16d0f8be7557835ee3524409b59c35c
glvega <gerardo.lopez@yuju.io>
2022-05-19 10:02:36 -0500

    Filtra por sucursal

--- pos_customs/static/src/js/ClientLine.js
+++ pos_customs/static/src/js/ClientLine.js
@@ -13,10 +13,11 @@ odoo.define('pos_custom_settle_due.ClientLine', function (require) {
                 if (this.props.selectedClient == this.props.partner) {
                     event.stopPropagation();
                 }
-                // console.log("Da clic")
-                // console.log(this.pos.company.id)
-                // console.log(this.props.partner.id)
-                // console.log(this.env.pos.db.partner_sorted)
+                console.log("Da clic")
+                console.log(this)
+                console.log(this.pos.company.id)
+                console.log(this.props.partner.id)
+                console.log(this.env.pos.db.partner_sorted)
 
                 company_id = this.pos.company.id
 
