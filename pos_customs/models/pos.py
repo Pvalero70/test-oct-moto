46b01d36d1b4e2813a202c8b4e0fd57b7056e6ad
IsaacCA <IsaacCA>
2022-05-20 10:12:49 -0500

    MOdf. Customer credit limit, split invoices first

--- pos_customs/models/pos.py
+++ pos_customs/models/pos.py
@@ -75,6 +75,74 @@ class PosOrder(models.Model):
 
         return vals
 
+    @api.model
+    def _split_invoice_vals_bk(self, invoice_data, quit_commissions=True, order=None):
+        """
+        This method process invoice data to generate two different sets; one for common lines and other for
+        lines that have bank commission products.
+        :param invoice_data: the original dataset
+        :param quit_commissions: to decide which lines must be removed. By default quit the bank commission lines.
+        :param order: pos order object.
+        :return: invoice data set without some lines, depends of  quit_commissions
+
+        Ejemplo de invoice data:
+                    {
+                    'invoice_origin': 'Lopez Mateos/0011',
+                    'journal_id': 32,
+                    'move_type': 'out_invoice',
+                    'ref': 'Lopez Mateos/0011',
+                    'partner_id': 11,
+                    'partner_bank_id': False,
+                    'currency_id': 33,
+                    'invoice_user_id': 2,
+                    'invoice_date': datetime.date(2022, 5, 18),
+                    'fiscal_position_id': False,
+                    'invoice_line_ids':
+                        [
+                        (0, None, {
+                            'product_id': 210,
+                            'quantity': 1.0,
+                            'discount': 0.0,
+                            'price_unit': 500.0,
+                            'name': 'BALATA',
+                            'tax_ids': [(6, 0, [14])],
+                            'product_uom_id': 1,
+                            'pos_order_line_id': 21
+                            }),
+                        (0, None, {
+                            'product_id': 219,
+                            'quantity': 1.0,
+                            'discount': 0.0,
+                            'price_unit': 35.0,
+                            'name': 'Comisión por uso de tarjeta',
+                            'tax_ids': [(6, 0, [])],
+                            'product_uom_id': 1,
+                            'pos_order_line_id': 22
+                            })
+                        ],
+
+                    'invoice_cash_rounding_id': False,
+                    'team_id': crm.team(5,),
+                    'partner_shipping_id': 11,
+                    'l10n_mx_edi_payment_method_id': 18,
+                    'l10n_mx_edi_usage': 'P01',
+                    'invoice_payment_term_id': '1'
+
+                    }
+
+        """
+        _log.info("\n Invoice data original :: \n\n%s\n\n " % invoice_data)
+        for pl in order.payment_ids:
+            # Se tienen que agrupar  por método de pago
+            if quit_commissions:
+                # quitamos las lineas de comisiones de invoice data.
+                pass
+            else:
+                # quitamos las lineas que no sean comisiones.
+                pass
+
+        return invoice_data
+
     def _generate_pos_order_invoice(self):
         moves = self.env['account.move']
 
@@ -87,11 +155,17 @@ class PosOrder(models.Model):
             if not order.partner_id:
                 raise UserError(_('Please provide a partner for the sale.'))
             move_vals = order._prepare_invoice_vals()
-            _log.info("\n ============== MOVE VALS to SPLIT :: %s" % move_vals)
+            # La idea es facturar por separado, creamos una copia de move_vals y
+            # al original le quitamos la o las lineas con algún producto que
+            move_vals_commissions = self._split_invoice_vals_bk(move_vals, quit_commissions=False, order=order)
+            move_vals = self._split_invoice_vals_bk(move_vals, quit_commissions=True, order=order)
+
 # Comentado por pruebas.
             new_move = order._create_invoice(move_vals)
+            # new_move_bc = order._create_invoice(move_vals_commissions)
             order.write({'account_move': new_move.id, 'state': 'invoiced'})
             new_move.sudo().with_company(order.company_id)._post()
+            # new_move_bc.sudo().with_company(order.company_id)._post()
             moves += new_move
             line_zerodays = new_move.invoice_payment_term_id.line_ids.filtered(lambda x: x.value_amount == 0 and x.days == 0 and x.option == "day_after_invoice_date")
             if line_zerodays:
@@ -100,6 +174,8 @@ class PosOrder(models.Model):
                 delta_days = new_move.invoice_payment_term_id.line_ids.filtered(lambda x: x.days > 0 and x.option == "day_after_invoice_date")[:1].days
                 new_move.invoice_date_due = fields.Date.today() + relativedelta(days=delta_days)
                 new_move._compute_l10n_mx_edi_payment_policy()
+
+            # Ya creada la factura de comisiones, aplicar las mismas validaciones anterires que aplican a la factura convencional.
 
         if not moves:
             return {}
