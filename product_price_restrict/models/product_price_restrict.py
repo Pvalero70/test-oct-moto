d02b6505a68de85e928a1be3c1c3656e555c2211
AledroG <alexgama9817@gmail.com>
2022-05-22 14:51:10 -0500

    Bug in product price

--- product_price_restrict/models/product_price_restrict.py
+++ product_price_restrict/models/product_price_restrict.py
@@ -9,11 +9,14 @@ class PosOrderC(models.Model):
     _inherit = "product.template"
 
 
-    edit_price_sale = fields.Boolean("Editar precio de venta", compute='_compute_group_edit_sale_price')
-    edit_coste = fields.Boolean("Editar precio de venta", compute='_compute_group_coste')
+    edit_price_sale = fields.Boolean("Editar precio de venta", compute='_compute_group_edit_sale_price', store=False)
+    edit_coste = fields.Boolean("Editar precio de venta", compute='_compute_group_coste',store=False)
+
 
     def _compute_group_edit_sale_price(self):
         self.edit_price_sale = self.env.user.has_group('product_price_restrict.product_sale_price_group')
+        _log.info("PRODUCT:: Grupo sale permiso %s", self.edit_price_sale)
 
     def _compute_group_coste(self):
         self.edit_coste = self.env.user.has_group('product_price_restrict.product_price_group')
+        _log.info("PRODUCT:: Grupo coste permiso %s", self.edit_coste)
