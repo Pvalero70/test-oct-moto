045b90abcf9a9d2d49e727a8d90332d21ee60f85
AledroG <alexgama9817@gmail.com>
2022-05-22 16:14:59 -0500

    Update product_price_restrict.py

--- product_price_restrict/models/product_price_restrict.py
+++ product_price_restrict/models/product_price_restrict.py
@@ -18,7 +18,9 @@ class PosOrderC(models.Model):
     @api.onchange("list_price")
     def _onchangeprice(self):
         _log.info("id del registro = %s",self.id)
-        if not self.env.user.has_group('product_price_restrict.product_sale_price_group'):
+        registro = self.env['product.template'].search([('id', '=', self.id)], limit=1)
+        _log.info("Registro = %s , len %s",registro,len(registro))
+        if registro and not self.env.user.has_group('product_price_restrict.product_sale_price_group'):
             raise ValidationError(_("Advertencia, no puedes modificar el precio de venta"))
 
     @api.onchange("standard_price")
