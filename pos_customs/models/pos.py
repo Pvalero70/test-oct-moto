337e43c961417cdf64d66416ec40aec35da95cef
IsaacCA <IsaacCA>
2022-05-24 20:23:46 -0500

    [I] ADD: payment to invoices for bc

--- pos_customs/models/pos.py
+++ pos_customs/models/pos.py
@@ -152,6 +152,17 @@ class PosOrder(models.Model):
             invoice_data['invoice_line_ids'] = new_invoice_line_ids
         return invoice_data
 
+    def _apply_invoice_payments_bc(self, invoice):
+        receivable_account = self.env["res.partner"]._find_accounting_partner(self.partner_id).property_account_receivable_id
+        # payment_moves = self.payment_ids._create_payment_moves()
+        # Buscar los pagos creados
+        invoice_receivable = self.account_move.line_ids.filtered(lambda line: line.account_id == receivable_account)
+        # Reconcile the invoice to the created payment moves.
+        # But not when the invoice's total amount is zero because it's already reconciled.
+        if not invoice_receivable.reconciled and receivable_account.reconcile:
+            payment_receivables = payment_moves.mapped('line_ids').filtered(lambda line: line.account_id == receivable_account)
+            (invoice_receivable | payment_receivables).reconcile()
+
     def _generate_pos_order_invoice(self):
         moves = self.env['account.move']
         for order in self:
@@ -168,14 +179,11 @@ class PosOrder(models.Model):
             move_vals = self._split_invoice_vals_bk(move_vals, quit_commissions=True, order=order)
 # Comentado por pruebas.
             new_move = order._create_invoice(move_vals)
-            _log.info("\n\n _____________________ Factura normal:: %s " % new_move)
+            new_move_bc = None
             if move_vals_commissions:
                 new_move_bc = order._create_invoice(move_vals_commissions)
                 new_move_bc.sudo().with_company(order.company_id)._post()
                 moves += new_move_bc
-                _log.info("\n\n _____________________ Factura de comision: :: %s " % new_move_bc)
-
-
             order.write({'account_move': new_move.id, 'state': 'invoiced'})
             new_move.sudo().with_company(order.company_id)._post()
             moves += new_move
@@ -186,6 +194,10 @@ class PosOrder(models.Model):
                 delta_days = new_move.invoice_payment_term_id.line_ids.filtered(lambda x: x.days > 0 and x.option == "day_after_invoice_date")[:1].days
                 new_move.invoice_date_due = fields.Date.today() + relativedelta(days=delta_days)
                 new_move._compute_l10n_mx_edi_payment_policy()
+            # Add payment to bc here. the payment moves must be created before here.
+            # if new_move_bc is not None:
+            #     self._apply_invoice_payments_bc(new_move_bc)
+
         if not moves:
             return {}
 
