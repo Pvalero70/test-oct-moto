187eb87dc8c141444e28ce1ffb97cc3abc95f86d
AledroG <alexgama9817@gmail.com>
2022-05-26 04:44:42 -0500

    Update product_category.py

--- credit_note_restrict/models/product_category.py
+++ credit_note_restrict/models/product_category.py
@@ -106,9 +106,8 @@ class AccountTranzientReversal(models.TransientModel):
         ids_lines.pop(0)
         lineasdiferentescategorias=[]
         for move in self.new_move_ids:
-            product_descuento = self.env['product.template'].sudo().search(
+            product_descuento = self.env['product.product'].search(
                 [('is_discount_product', '=', True), ('company_id', '=', move.company_id.id)], limit=1)
-            _logger.info("Producto de descuento %s",product_descuento)
             if not product_descuento:
                 raise ValidationError(_("No se ha elegido un producto para tomar como descuento en notas de credito"))
 
