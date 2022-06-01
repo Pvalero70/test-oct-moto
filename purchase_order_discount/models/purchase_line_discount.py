3b5caa455228ab40ced8b14a1b2c2235225856f4
AledroG <alexgama9817@gmail.com>
2022-05-31 22:36:25 -0500

    Function onchange discount

--- purchase_order_discount/models/purchase_line_discount.py
+++ purchase_order_discount/models/purchase_line_discount.py
@@ -5,7 +5,6 @@ from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError, Warning
 from lxml import etree
 
-
 _logger = logging.getLogger(__name__)
 
 
@@ -14,12 +13,10 @@ class PurchaseOrderLineDiscount(models.Model):
 
     discount = fields.Float('Descuento %')
 
-    @api.onchange('discount','price_unit','product_qty')
+    @api.onchange('discount', 'price_unit', 'product_qty')
     def _compute_discount_subtotal(self):
         _logger.info("Descuento computar")
         subtotal = self.price_unit * self.product_qty
-        total = subtotal - (subtotal * (self.discount/100))
-        _logger.info("total %s",self.total)
-        self.write({'price_subtotal':total})
-
-
+        total = subtotal - (subtotal * (self.discount / 100))
+        _logger.info("total %s", total)
+        self.write({'price_subtotal': total})
