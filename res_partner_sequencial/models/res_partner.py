4b76f258d7be2e15381adb4f7f70554c69b1fa74
AledroG <alexgama9817@gmail.com>
2022-06-04 17:02:24 -0500

    Update res_partner.py

--- res_partner_sequencial/models/res_partner.py
+++ res_partner_sequencial/models/res_partner.py
@@ -17,11 +17,15 @@ class ResPartnertInherit(models.Model):
             seq = ""
             if rec.supplier_rank > 0: #Es un proveedor
                 seq = rec.env['ir.sequence'].next_by_code('res.partner.proveedor.sequence')
+                rec.sequencial_code_prov = seq
+                _logger.info("Proveedor Sequencial = %s", seq)
 
             if rec.customer_rank > 0: #es un cliente
                 seq = rec.env['ir.sequence'].next_by_code('res.partner.cliente.sequence')
-            _logger.info("Sequencial = %s",seq)
-            rec.sequencial_code_z = seq
+                rec.sequencial_code_client = seq
+                _logger.info("Cliente Sequencial = %s", seq)
+
+
 
 
     sequencial_code_prov = fields.Char(string="Sequencial Cliente", readonly=True, required=True, compute='_default_seq_code',store=True)
