8b41815671432b5942df1a5c937bc50c2529a215
AledroG <alexgama9817@gmail.com>
2022-06-11 14:43:58 -0500

    Domain in products reparaciones

--- repair_mechanic/models/__init__.py
+++ repair_mechanic/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import repair_mechanic
+from . import historial_reparaciones
---
+++ repair_mechanic/models/historial_reparaciones.py
@@ -0,0 +1,24 @@
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError, UserError, Warning
+
+_logger = logging.getLogger(__name__)
+
+
+class RepairMechanic(models.Model):
+    _inherit = 'repair.order'
+
+    product_id = fields.Many2one(
+        'product.product', string='Product to Repair',
+        domain='_domain_package_type',
+        readonly=True, required=True, states={'draft': [('readonly', False)]}, check_company=True)
+
+    #        domain="[('type', 'in', ['product', 'consu']), '|', ('company_id', '=', company_id), ('company_id', '=', False)]",
+
+    @api.model
+    def _domain_package_type(self):
+
+        product = self.env['product.product'].search([('type', 'in', ['product', 'consu']), ('company_id', 'in', [self.env.company.id, False])], limit=3)
+        _logger.info("Domain %s",product)
+        return [('id', 'in', product.ids)]
