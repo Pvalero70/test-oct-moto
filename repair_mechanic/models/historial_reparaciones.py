535ee272eb60cff27148bd6415d8a6e9069b64bd
AledroG <alexgama9817@gmail.com>
2022-06-11 19:58:09 -0500

    Domain in products reparaciones

--- repair_mechanic/models/historial_reparaciones.py
+++ repair_mechanic/models/historial_reparaciones.py
@@ -18,10 +18,11 @@ class RepairMechanic(models.Model):
     @api.onchange('partner_id')
     def _products_order(self):
         _logger.info("En onchange partner")
-        products = self.env['product.product'].search([('type', 'in', ['product', 'consu']),('company_id', 'in', [self.env.company.id,'',None,False])], limit=3)
+        products = self.env['product.product'].search([('type', 'in', ['product', 'consu']),('company_id', 'in', [self.env.company.id,'',None,False])])
         ordenes_ventas = self.env['pos.order'].search([('partner_id','=',self.partner_id.id),('state','in',['done','invoiced'])])
         productos_ventas = [product for orden in ordenes_ventas for line in orden.line for product in line.product_id ]
         _logger.info("Domain %s",products)
+        _logger.info("Ventas %s",productos_ventas)
         for product in products:
             product_encontrado = False
             for product_venta in productos_ventas:
