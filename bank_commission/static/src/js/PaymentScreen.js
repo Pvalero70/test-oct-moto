93a5f9b5b5e76481643d9196062f45f8a58ac304
AledroG <alexgama9817@gmail.com>
2022-06-15 09:32:13 -0500

    Changes in IsaacDev

--- bank_commission/static/src/js/PaymentScreen.js
+++ bank_commission/static/src/js/PaymentScreen.js
@@ -8,7 +8,7 @@ const Registries = require('point_of_sale.Registries');
 var rpc = require('web.rpc');
 var exports = require("point_of_sale.models");
 
-exports.load_fields('pos.payment.method', ["bank_commission_product_id", "bank_commission_amount", "bank_commission_method"])
+exports.load_fields('pos.payment.method', ["bank_commission_product_id", "bank_commission_amount", "bank_commission_method", "product_cate_commission_ids"])
 
 const PaymentScreenBc = (PaymentScreen) =>
     class extends PaymentScreen{
@@ -18,28 +18,57 @@ const PaymentScreenBc = (PaymentScreen) =>
 
         addNewPaymentLine({ detail: paymentMethod }) {
             if (paymentMethod.bank_commission_method){
-                let product_id = paymentMethod.bank_commission_product_id[0];
-                let product_byid = this.env.pos.db.get_product_by_id(product_id);
-                let oline = this.currentOrder.get_orderlines().find(line => line.product.id === product_id);
-                let current_com = 0;
-                if (oline && this.currentOrder.get_due() > 0){
-                    current_com = oline.price;
-                }
-                let price = 0;
-                if (paymentMethod.bank_commission_method == "percentage"){
-                    let total_due = this.currentOrder.get_total_with_tax()-current_com-this.currentOrder.get_total_paid()+ this.currentOrder.get_rounding_applied();
-                    price = total_due * (paymentMethod.bank_commission_amount/100);
-//                    price = this.currentOrder.get_due() * (paymentMethod.bank_commission_amount/100);
-                }
-                if (paymentMethod.bank_commission_method == "fixed"){
-                    price = paymentMethod.bank_commission_amount;
-                }
-                this.currentOrder.add_product(product_byid, {
-                    quantity: 1,
-                    price: price,
-                    lst_price: price,
-                    extras: {price_manually_set: true,paymentMethod:paymentMethod.id},
+
+                // Decide if add commission or not.
+                /* Solo debe afectar a los productos motos de Vehículos TT y en las demás compañias debe afectar al resto si afecta a otros productos. */
+                /*
+                console.log("MÉTodo de pago :: ");
+                console.log(paymentMethod);
+                console.log("LINEAS DEL POS ORDER ACTUAL:: ");
+                console.log(this.currentOrder.get_orderlines());
+                */
+                let product_categ_id_list = [];
+                // let product_categ_parents_id_list = [];
+                this.currentOrder.get_orderlines().forEach(element => {
+                    if(product_categ_id_list.includes(element.product.categ.id) == false){
+                        product_categ_id_list.push(element.product.categ.id);
+                    }
+                    if(product_categ_id_list.includes(element.product.categ.parent_id[0]) == false){
+                        product_categ_id_list.push(element.product.categ.parent_id[0]);
+                    }
+
                 });
+//                console.log("Lista de ids de categorias de produtos y sus padres. ");
+//                console.log(product_categ_id_list);
+                let intersection = paymentMethod.product_cate_commission_ids.filter(element => product_categ_id_list.includes(element));
+                console.log("Coincidencias de categorias de prod:: ");
+                console.log(intersection);
+                if(intersection.length >= 1){
+                    let product_id = paymentMethod.bank_commission_product_id[0];
+                    let product_byid = this.env.pos.db.get_product_by_id(product_id);
+                    let oline = this.currentOrder.get_orderlines().find(line => line.product.id === product_id);
+                    let current_com = 0;
+                    if (oline && this.currentOrder.get_due() > 0){
+                        current_com = oline.price;
+                    }
+                    let price = 0;
+                    if (paymentMethod.bank_commission_method == "percentage"){
+                        let total_due = this.currentOrder.get_total_with_tax()-current_com-this.currentOrder.get_total_paid()+ this.currentOrder.get_rounding_applied();
+                        price = total_due * (paymentMethod.bank_commission_amount/100);
+                        //price = this.currentOrder.get_due() * (paymentMethod.bank_commission_amount/100);
+                    }
+                    if (paymentMethod.bank_commission_method == "fixed"){
+                        price = paymentMethod.bank_commission_amount;
+                    }
+
+                    this.currentOrder.add_product(product_byid, {
+                        quantity: 1,
+                        price: price,
+                        lst_price: price,
+                        extras: {price_manually_set: true,paymentMethod:paymentMethod.id},
+                    });
+                }
+
             }
             return super.addNewPaymentLine(...arguments);
         }
