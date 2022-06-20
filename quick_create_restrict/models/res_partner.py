051c74eeee15fa3bbf7c4fc66b258666e34f5839
AledroG <alexgama9817@gmail.com>
2022-06-20 13:16:02 -0500

    Debug:: no create, no quick create odoo 15

---
+++ quick_create_restrict/models/res_partner.py
@@ -0,0 +1,29 @@
+import logging
+from collections import defaultdict
+
+
+from odoo import _, api, Command, fields, models
+from lxml import etree
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartnerState(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(ResPartnerState, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar,
+                                                       submenu=submenu)
+
+        doc = etree.XML(res['arch'])
+        nodes = doc.xpath("//field[@name='state_id']")
+
+
+        for node in nodes:
+            node.set('options', "{'no_quick_create':True,'no_create_edit':True,'no_open': True,'no_create': True}")
+
+        res['arch'] = etree.tostring(doc)
+        _logger.info("Etree field state_id")
+        return res
+
