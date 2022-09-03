49e42dbc27aa1de7a258b8c66f6b777d913c97aa
glvega <gerardo.lopez@yuju.io>
2022-09-02 21:27:56 -0500

    Agrega modulo costo serial

--- cost_serial_number/__manifest__.py
+++ cost_serial_number/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sales', 'purchase', 'stock', 'purchase_stock', 'inventory_customs'],
+    'depends': ['sale_stock_margin'],
 
     # always loaded
     'data': [
--- cost_serial_number/models/models.py
+++ cost_serial_number/models/models.py
@@ -9,6 +9,12 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class SaleOrder(models.Model):
-	_inherit = "sale.order"
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+	@api.depends('move_ids', 'move_ids.stock_valuation_layer_ids', 'order_id.picking_ids.state')
+	def _compute_purchase_price(self):
+		_logger.info("### OVERRIDE PURCHASE PRICE ###")
+		for line in self:
+			line.purchase_price = 50
 
