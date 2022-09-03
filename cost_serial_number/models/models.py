7d76365c554756d5a1486134e097de90cc0a176f
glvega <gerardo.lopez@yuju.io>
2022-09-02 21:42:06 -0500

    Agrega modulo costo serial

--- cost_serial_number/models/models.py
+++ cost_serial_number/models/models.py
@@ -16,5 +16,17 @@ class SaleOrderLine(models.Model):
 	def _compute_purchase_price(self):
 		_logger.info("### OVERRIDE PURCHASE PRICE ###")
 		for line in self:
+			
+			if line.lot_id:
+				move_line = self.env['stock.move.line'].search([('lot_id', '=', line.lot_id.id)])
+				if move_line:
+					_logger.info("Se obtuvo la linea del movimiento")
+					move = move_line.move_id
+					if move.purchase_line_id:
+						_logger.info("Se obtuvo el precio unitario de la compra")
+						price_unit = move.purchase_line_id
+						line.purchase_price = price_unit
+						return
+
 			line.purchase_price = 50
 
